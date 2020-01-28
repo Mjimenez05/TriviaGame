4d418f86aebfe105c9bedb2123ce82ac816a379f
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-01-28 12:40:16 -0700

    Working in the css making the box work, Created hover button and started the java coding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,27 @@
-//  Need to have a score counter
-//  Create questions for the game
-//  Need to have multiple choice for the questions 
-//
+// select all elements
+const start = document.getElementById("start");
+// create our questions
+// create some variables
+// render a question
+function renderQuestion() {
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>" + q.question + "</p>";
+    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+start.addEventListener("click", startQuiz);
+// start quiz
+function startQuiz() {
+    start.style.display = "none";
+    renderQuestion
+}
+// render progress
+// counter render
+// checkAnwer
+// answer is correct
+// answer is Wrong
+// score render
+// calculate the right answers
