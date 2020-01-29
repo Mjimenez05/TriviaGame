1aabaf307341c4eac0c5328ac863d51a9a87ba04
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-01-28 23:54:17 -0700

    tried a couple different versions of this game, landed on this one. will continue coding, needs to be turned in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,41 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
     // start quiz
 function startGame() {
-
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 // next question
 function setNextQuestion() {
-
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question) {}
 
 function selectAnswer(params) {
 
 }
 
-// check Answer
+const questions = [{
+    question: 'What color is Darth Vaders Light Saber?',
+    answers: [
+        { text: 'Red', correct: true } { text: 'Green', correct: false } { text: 'Yellow', correct: false } { text: 'Blue', correct: false }
+    ]
+}]
 
 // answer is correct
-// change progress color to green
+// changecolor to green
 
 // answer is wrong
-// change progress color to red
+// changecolor to red
 
 // end the quiz and show the score
 
